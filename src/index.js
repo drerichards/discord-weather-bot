64f9c2d10bf34da77a6ca4fcc276ee8b88426c92
Andre Richards <andrerichards@AndresMBPBlack.attlocal.net>
2024-01-18 01:03:57 -0500

    initial setup to discord

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ]
+})
+
+client.on(Events.ClientReady, () => {
+    console.log('Logged in')
+})
+
+client.login(process.env.DISCORD_TOKEN)
