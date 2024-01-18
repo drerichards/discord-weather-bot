d95c5e9f748b45848b1e40fe9108d7446f545a72
Andre Richards <13812497+drerichards@users.noreply.github.com>
2024-01-18 04:45:51 -0500

    recommit

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,12 +1,8 @@
 const interactionCreateHandler = async (interaction) => {
-  if (!interaction.isChatInputCommand()) {
-    return;
-  }
+  if (!interaction.isChatInputCommand()) return;
 
   const command = interaction.client.commands.get(interaction.commandName);
-  if (!command) {
-    return;
-  }
+  if (!command) return;
 
   try {
     await command.execute(interaction);
